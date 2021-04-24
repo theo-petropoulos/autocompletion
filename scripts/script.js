c0698633931e6f4d6151a460ab2c2cb6ab9401b1
Theo <theo.petropoulos@laplateforme.io>
2021-04-25 00:17:19 +0200

    add arrow + enter controls

--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,84 @@
 window.search='';
+window.selector=0;
 
 $(function(){
     //Set focus on search bar
     $("#search_input").focus();
 
     //Catch enter and delete keys
-    $("#search_input").on({
+    $(document).on({
         'keydown':function(e){
-            if(e.which=='13'){
-                e.preventDefault();
-                $("#search_submit").trigger('click');
+            if(e.which=='13' && selector==0){
+                $("#search_submit").trigger('click')
+            }
+            else if(e.which=='13' && selector!==0){
+                e.preventDefault()
+                clog($(".searchp:nth-child("+selector+")"))
+                $("#search_input").val($("#search_input").val() + $(".searchp:nth-child("+selector+")").find('span').html())
+                $("#search_submit").trigger('click')
             }
             if(e.which=='46' || e.which=='8'){
                 //
                 // ACTION ON DELETE
                 //
             }
+            // if(e.keyCode==40){
+            //     e.preventDefault()
+            //     $(document).find('#searchp2').trigger('mouseenter')
+            // }
         },
         'keyup':function(e){
-            let clear=setTimeout(() => {
-                $("#results_box").empty();
-            }, 40);
-            complete = $('#search_input').val();
-            if(complete.length>0){
-                var post=$.post('/autocompletion/php/search.php',
-                {
-                    complete:complete
-                })
-                .done(function(data,status){
-                    try{
-                        let results=JSON.parse(data);
-                        $("#results_box p").remove();
-                        $(results).each(function(arrkey, object){
-                            $("#results_box").prepend(
-                                '<p class="searchp" id="searchp' + arrkey + '">' + complete + '<span class="chars_left">' + object['nom'].substr(complete.length) + '</span></p>'
-                            );
-                        clearTimeout(clear);
-                        });
-                    }catch (e){
-                        return false;
-                    }
-                    if($("#results_box p").length>1){
-                        $("#search_input").css({
-                            "border-bottom-right-radius":"0",
-                            "border-bottom-left-radius":"0"
-                        });
-                    }
-                })
-                .fail(function(){
-                    clog('not sent')
-                })
+            if(e.keyCode==40){
+                if($('.searchp').length > selector) selector++;
+                e.preventDefault()
+                $('.searchp:nth-child('+(selector-1)+')').css('background', 'initial')
+                $('.searchp:nth-child('+selector+')').css('background', 'rgb(240, 240, 240)')
+            }
+            else if(e.keyCode==38){
+                if(selector > 0) selector--;
+                e.preventDefault()
+                $('.searchp:nth-child('+(selector+1)+')').css('background', 'initial')
+                $('.searchp:nth-child('+selector+')').css('background', 'rgb(240, 240, 240)')
+            }
+            else{
+                let clear=setTimeout(() => {
+                    $("#results_box").empty();
+                }, 40);
+                complete = $('#search_input').val();
+                if(complete.length>0){
+                    var post=$.post('/autocompletion/php/search.php',
+                    {
+                        complete:complete
+                    })
+                    .done(function(data,status){
+                        try{
+                            let results=JSON.parse(data);
+                            $("#results_box p").remove();
+                            $(results).each(function(arrkey, object){
+                                $("#results_box").prepend(
+                                    '<p class="searchp" id="searchp' + arrkey + '">' + complete + '<span class="chars_left">' + object['nom'].substr(complete.length) + '</span></p>'
+                                );
+                            clearTimeout(clear);
+                            });
+                        }catch (e){
+                            return false;
+                        }
+                        if($("#results_box p").length>1){
+                            $("#search_input").css({
+                                "border-bottom-right-radius":"0",
+                                "border-bottom-left-radius":"0"
+                            });
+                        }
+                    })
+                    .fail(function(){
+                        clog('not sent')
+                    })
+                }
             }
         },
-        'blur':function(e){
-            $(document).unbind().on('click', function(e){
+        'blur':function(){
+            selector=0;
+            $(document).off('click').on('click', function(e){
                 if($(e.target).attr('id')!==$("#search_input").attr('id')) $("#results_box").empty();
                 if($(e.target).is('.searchp, .searchp span')){
                     $(e.target).is('span') ? 
@@ -66,14 +91,25 @@ $(function(){
                 "border-bottom-left-radius":"8px"
             });
         },
-        'focus':function(e){
+        'focus':function(){
             if(search!==undefined) $("#search_input").trigger('keyup');
         }
-    });
+    }, "#search_input");
 
-    //Prevent form submit
-    $("#search_form").on('submit', function(e){
-    });
+    $(document).on({
+        'mouseenter': function(){
+            $(this).css({
+                "background":"rgb(240, 240, 240)",
+                "transition":"0.3s"
+            })
+        },
+        'mouseleave': function(){
+            $(this).css({
+                "background":"initial",
+                "transition":"0.3s"
+            })
+        }
+    }, '.searchp')
 });
 
 function clog(x){
